d665faa118b1fd38a3bfcbebba29a9372f8d2f04
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-27 22:27:42 -0300

    Closure

--- programacao_funcional/decorador/closure.py
+++ programacao_funcional/decorador/closure.py
@@ -1,9 +1,11 @@
-def fabrica_de_multiplicadores():
-    def dobro(n):
-        return n * 2
+def fabrica_de_multiplicadores(multiplicador):
+    def multiplicar(n):
+        return n * multiplicador
 
-    return dobro
+    return multiplicar
 
 
-dobro_externo = fabrica_de_multiplicadores()
-print(dobro_externo(3))
+dobro = fabrica_de_multiplicadores(2)
+triplo = fabrica_de_multiplicadores(3)
+print(dobro(3))
+print(triplo(4))
