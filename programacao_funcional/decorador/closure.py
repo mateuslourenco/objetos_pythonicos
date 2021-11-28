5a8280ef03d2024a84e13cf74481b7dc581210c4
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-27 22:31:27 -0300

    teste multiplicar_dividir

--- programacao_funcional/decorador/closure.py
+++ programacao_funcional/decorador/closure.py
@@ -5,7 +5,17 @@ def fabrica_de_multiplicadores(multiplicador):
     return multiplicar
 
 
+def fabrica_de_multiplicadores_e_divisores(multiplicador, divisor):
+    def multiplicar_dividir(n):
+        return n * multiplicador / divisor
+
+    return multiplicar_dividir
+
+
 dobro = fabrica_de_multiplicadores(2)
 triplo = fabrica_de_multiplicadores(3)
 print(dobro(3))
 print(triplo(4))
+
+teste = fabrica_de_multiplicadores_e_divisores(10, 5)
+print(teste(1))
