1bc0a54f9f42d3b3a020720f0e0379a68dc474f5
mateus <mateuslourenco55@outlook.com>
2022-02-15 19:02:28 -0300

    Implementada classe trem

---
+++ aulas_gravadas/06-Iteração e Protocolo de Sequência/trem_seq.py
@@ -0,0 +1,21 @@
+from collections import Sequence
+
+
+class Trem(Sequence):
+    def __init__(self, num_vagoes):
+        self.num_vagoes = num_vagoes
+
+    def __len__(self):
+        return self.num_vagoes
+
+    def __getitem__(self, pos):
+        indice = pos if pos >= 0 else self.num_vagoes + pos
+        if 0 <= indice < self.num_vagoes:
+            # indice 2 -> vagao #3
+            return f'vagao #{indice + 1}'
+        raise IndexError(f'Vagao inexistente {pos}')
+
+
+if __name__ == '__main__':
+    for vagao in Trem(4):
+        print(vagao)
