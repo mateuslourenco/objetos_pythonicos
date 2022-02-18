44eab77fcd9e7130a9a02129c1234f46c0ff1657
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-18 00:35:07 -0300

    metodos

---
+++ aulas_gravadas/10-Property e Classes Abstratas/bicicletaAbstract.py
@@ -0,0 +1,53 @@
+class Bicicleta:
+    _marca = 'Caloi'
+
+    def __init__(self):
+        self._velocidade = 0
+
+    @classmethod
+    def marca(cls):
+        return cls._marca
+
+    @staticmethod
+    def rodas():
+        return 2
+
+    @property
+    def velocidade(self):
+        return self._velocidade
+
+    @velocidade.setter
+    def velocidade(self, valor):
+        if valor >= 0:
+            self._velocidade = valor
+        else:
+            self._velocidade = 0
+
+    def pedalar(self):
+        raise NotImplementedError
+
+    def frear(self):
+        raise NotImplementedError
+
+
+class Monark(Bicicleta):
+    _marca = 'Monark'
+
+    def pedalar(self):
+        self.velocidade += 10
+
+    def frear(self):
+        self.velocidade -= 3
+
+
+if __name__ == '__main__':
+    bicicleta = Monark()
+    bicicleta.pedalar()
+    bicicleta.frear()
+    bicicleta.frear()
+    bicicleta.frear()
+    bicicleta.frear()
+    print(bicicleta.velocidade)
+    print(Bicicleta.marca())
+    print(bicicleta.rodas())
+    print(Monark.marca())
